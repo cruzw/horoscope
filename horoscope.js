ce80835fd46591a7f3b3bf184407988642928352
Geoffrey Cruz Welborn <gcruzwelborn@gmail.com>
2015-11-23 13:16:30 -0800

    added getZodiac fn
    takes a 4 digit birth year, spits out Zodiac animal

--- horoscope.js
+++ horoscope.js
@@ -88,3 +88,22 @@ exports.getSign = function (month, day) {
         }
     }
 };
+
+exports.getZodiac = function (year) {
+    'use strict';
+    remainderAnimals = {
+        0: "Monkey",
+        1: "Rooster",
+        2: "Dog",
+        3: "Pig",
+        4: "Rat",
+        5: "Ox",
+        6: "Tiger",
+        7: "Rabbit",
+        8: "Dragon",
+        9: "Snake",
+        10: "Horse",
+        11: "Goat"
+    };
+    return remainderAnimals[year % 12];
+};
