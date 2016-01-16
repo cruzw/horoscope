395de1fe0d421d8a3336c7a5d5226409950ea19e
gcwelborn <gcruzwelborn@gmail.com>
2016-01-15 19:57:01 -0800

    added proper unit tests, refactored getSign

--- horoscope.js
+++ horoscope.js
@@ -1,126 +1,116 @@
 /**
  * Get Astrological Sign from Birthday MONTH and Birthday DAY
- * @throws {Error} if month is not between 1-12 
- * @throws {Error} if day is not between 1-31 
+ * @throws {Error} if month is not between 1-12
+ * @throws {Error} if day is not between 1-31
  * @param   {number} month birth month
  * @param   {number} day   birth day
  * @returns {string} Astrological Sign
  */
 
-exports.getSign = function (month, day) {
-   'use strict';
-   if (month < 1 || month > 12) {
-      throw new Error("month needs to be between 1-12");
-   }
-   if (day < 1 || day > 31) {
-      throw new Error("check that day isn't zero, negative, or greater than 31");
-   }
-   
-   
-   if (month <= 6 && month > 0) { //months: Jan, Feb, March, Apr, May, June
-      switch (month) {
-      case 1: //January
-         if (day <= 19) {
-            return "Capricorn";
-         } else {
-            return "Aquarius";
-         }
-      case 2: //February
-         if (day <= 19) {
-            return "Aquarius";
-         } else {
-            return "Pisces";
-         }
-      case 3: //March
-         if (day <= 20) {
-            return "Pisces";
-         } else {
-            return "Aries";
-         }
-      case 4: //April
-         if (day <= 20) {
-            return "Aries";
-         } else {
-            return "Taurus";
-         }
-      case 5: //May
-         if (day <= 20) {
-            return "Taurus";
-         } else {
-            return "Gemini";
-         }
-      case 6: //June
-         if (day <= 20) {
-            return "Gemini";
-         } else {
-            return "Cancer";
-         }
-      }
-   } else if (month > 6) { //months: July, Aug, Sept, Oct, Nov, Dec
-      switch (month) {
-      case 7: //July
-         if (day <= 22) {
-            return "Cancer";
-         } else {
-            return "Leo";
-         }
-      case 8: //August
-         if (day <= 22) {
-            return "Leo";
-         } else {
-            return "Virgo";
-         }
-      case 9: //September
-         if (day <= 22) {
-            return "Virgo";
-         } else {
-            return "Libra";
-         }
-      case 10: //October
-         if (day <= 22) {
-            return "Libra";
-         } else {
-            return "Scorpio";
-         }
-      case 11: //November
-         if (day <= 21) {
-            return "Scorpio";
-         } else {
-            return "Sagittarius";
-         }
-      case 12: //December
-         if (day <= 21) {
-            return "Sagittarius";
-         } else {
-            return "Capricorn";
-         }
-      }
-   }
+exports.getSign = function(month, day) {
+	'use strict';
+  let sign;
+	if (month < 1 || month > 12) throw new Error("month needs to be between 1-12")
+	if (day < 1 || day > 31) throw new Error("check that day isn't zero, negative, or greater than 31")
+	let months = {
+		1: "January",
+		2: "February",
+		3: "March",
+		4: "April",
+		5: "May",
+		6: "June",
+		7: "July",
+		8: "August",
+		9: "September",
+		10: "October",
+		11: "November",
+		12: "December"
+	}
+	let monthFunctions = {
+			January: (day) => {
+				(day <= 19) ? sign = "Capricorn": sign = "Aquarius"
+        return sign;
+			},
+			February: (day) => {
+				(day <= 18) ?
+				sign = "Aquarius": sign = "Pisces"
+        return sign;
+			},
+			March: (day) => {
+				(day <= 20) ?
+				sign = "Pisces": sign = "Aries"
+        return sign;
+			},
+			April: (day) => {
+				(day <= 19) ?
+				sign = "Aries": sign = "Taurus"
+        return sign;
+			},
+			May: (day) => {
+				(day <= 20) ?
+				sign = "Taurus": sign = "Gemini"
+        return sign;
+			},
+			June: (day) => {
+				(day <= 20) ?
+				sign = "Gemini": sign = "Cancer"
+        return sign;
+			},
+			July: (day) => {
+				(day <= 22) ?
+				sign = "Cancer": sign = "Leo"
+        return sign;
+			},
+			August: (day) => {
+				(day <= 22) ?
+				sign = "Leo": sign = "Virgo"
+        return sign;
+			},
+			September: (day) => {
+				(day <= 22) ?
+				sign = "Virgo": sign = "Libra"
+        return sign;
+			},
+			October: (day) => {
+				(day <= 22) ?
+				sign = "Libra": sign = "Scorpio"
+        return sign;
+			},
+			November: (day) => {
+				(day <= 21) ?
+				sign = "Scorpio": sign = "Sagittarius"
+        return sign;
+			},
+			December: (day) => {
+				(day <= 21) ?
+				sign = "Sagittarius": sign = "Capricorn"
+        return sign;
+			}
+		} //End of MonthFunc;
+	return monthFunctions[months[month]](day)
 };
 
 /**
  * Gets Zodiac sign from birth year
- * @param   {number} year birth year
+ * @param {number} year birth year
  * @returns {string} Zodiac Animal
  */
+// Should also account for day of chinese new year (since it changes but is in february)
 
- // Should also account for day of chinese new year (since it changes but is in february)
-
-exports.getZodiac = function (year) {
-   'use strict';
-   var remainderAnimals = {
-      0: "Monkey",
-      1: "Rooster",
-      2: "Dog",
-      3: "Pig",
-      4: "Rat",
-      5: "Ox",
-      6: "Tiger",
-      7: "Rabbit",
-      8: "Dragon",
-      9: "Snake",
-      10: "Horse",
-      11: "Goat"
-   };
-   return remainderAnimals[year % 12];
+exports.getZodiac = function(year) {
+	'use strict';
+	return {
+		0: "Monkey",
+		1: "Rooster",
+		2: "Dog",
+		3: "Pig",
+		4: "Rat",
+		5: "Ox",
+		6: "Tiger",
+		7: "Rabbit",
+		8: "Dragon",
+		9: "Snake",
+		10: "Horse",
+		11: "Goat"
+	}[year % 12]
 };
--- tape.js
+++ tape.js
@@ -1,10 +1,116 @@
-var test = require('tape');
-var horoscope = require('./horoscope');
+'use strict'
+let test = require('tape');
+let horoscope = require('./horoscope');
 
-horoscope.getSign(7, 25) //'Leo'
+///////////////////////       getSign      ///////////////////////////
 
-test('check Leo', function (t) {
-    t.plan(1);
-    t.equal(horoscope.getSign(7, 25), 'Leo');
-    t.end();
-});
+let borderDates = {
+	"January": [
+		[19, 'Capricorn'],
+		[20, 'Aquarius']
+	],
+	"February": [
+		[18, 'Aquarius'],
+		[19, 'Pisces']
+	],
+	"March": [
+		[20, 'Pisces'],
+		[21, 'Aries']
+	],
+	"April": [
+		[19, 'Aries'],
+		[20, 'Taurus']
+	],
+	"May": [
+		[20, 'Taurus'],
+		[21, 'Gemini']
+	],
+	"June": [
+		[20, 'Gemini'],
+		[21, 'Cancer']
+	],
+	"July": [
+		[22, 'Cancer'],
+		[23, 'Leo']
+	],
+	"August": [
+		[22, 'Leo'],
+		[23, 'Virgo']
+	],
+	"September": [
+		[22, 'Virgo'],
+		[23, 'Libra']
+	],
+	"October": [
+		[22, 'Libra'],
+		[23, 'Scorpio']
+	],
+	"November": [
+		[21, 'Scorpio'],
+		[22, 'Sagittarius']
+	],
+	"December": [
+		[21, 'Sagittarius'],
+		[22, 'Capricorn']
+	]
+}
+
+let monthNums = {
+	"January": 1,
+	"February": 2,
+	"March": 3,
+	"April": 4,
+	"May": 5,
+	"June": 6,
+	"July": 7,
+	"August": 8,
+	"September": 9,
+	"October": 10,
+	"November": 11,
+	"December": 12,
+}
+
+for (let month in borderDates) {
+  console.log(month)
+	test('check ' + month, function(t) {
+		t.plan(2)
+		let monthNum = monthNums[month];
+
+		let firstTestSign = borderDates[month][0][1]
+		let firstTestDay = borderDates[month][0][0]
+		t.equal(horoscope.getSign(monthNum, firstTestDay), firstTestSign);
+
+		let secondTestSign = borderDates[month][1][1]
+		let secondTestDay = borderDates[month][1][0]
+		t.equal(horoscope.getSign(monthNum, secondTestDay), secondTestSign);
+
+		t.end();
+	})
+}
+
+///////////////////////       getZodiac      ///////////////////////////
+
+let animalsToTest = {
+	'Rat': [1924, 1936, 1948, 1960, 1972, 1984, 1996, 2008, 2020],
+	'Ox': [1925, 1937, 1949, 1961, 1973, 1985, 1997, 2009, 2021],
+	'Tiger': [1926, 1938, 1950, 1962, 1974, 1986, 1998, 2010, 2022],
+	'Rabbit': [1927, 1939, 1951, 1963, 1975, 1987, 1999, 2011, 2023],
+	'Dragon': [1928, 1940, 1952, 1964, 1976, 1988, 2000, 2012, 2024],
+	'Snake': [1929, 1941, 1953, 1965, 1977, 1989, 2001, 2013, 2025],
+	'Horse': [1930, 1942, 1954, 1966, 1978, 1990, 2002, 2014, 2026],
+	'Goat': [1931, 1943, 1955, 1967, 1979, 1991, 2003, 2015, 2027],
+	'Monkey': [1932, 1944, 1956, 1968, 1980, 1992, 2004, 2016, 2028],
+	'Rooster': [1933, 1945, 1957, 1969, 1981, 1993, 2005, 2017, 2029],
+	'Dog': [1934, 1946, 1958, 1970, 1982, 1994, 2006, 2018, 2030],
+	'Pig': [1935, 1947, 1959, 1971, 1983, 1995, 2007, 2019, 2031],
+}
+
+for (let sign in animalsToTest) {
+	test('check ' + sign, function(t) {
+		t.plan(9)
+		animalsToTest[sign].forEach(function(year) {
+			t.equal(horoscope.getZodiac(year), sign);
+		})
+		t.end();
+	})
+}
